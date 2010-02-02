9bdce6127f925de128d3c80c135a4742843cfc2c
dansv693 <dansv693@student.liu.se>
2010-02-02 16:54:55 +0100

    viewimage klart

--- src/viewimage.cpp
+++ src/viewimage.cpp
@@ -4,6 +4,7 @@
 #include "arg.h"
 
 #include <iostream>
+#include <sstream>
 #include <string>
 #include <stdlib.h>
 #include <map>
@@ -18,17 +19,45 @@ int main()
 
    std::map<string,string> args = get_arguments(getenv("QUERY_STRING"));
 
-   cout << "<HTML><BODY>"                      << endl      
-        << "The parameters are: " << endl;
-
-
-   std::map<string,string>::iterator it = args.begin();
-   for(; it != args.end(); ++it)
-      cout << it->first << "\t: " << it->second << endl;
+   //Get the id of the image as a string
+   istringstream ss(args["id"]);
+   int id = -1;
+   ss >> id;
    
+   cout << "<HTML>\n"
+        << "<BODY>\n";
+
+   
+   if(id > 0)
+   {
+      try
+      {
+         CardDatabase db;
+         auto_ptr<Card> card = db.GetCard(id);     
+         cout << "<IMG src=\"" << card->URL << "\" />" << endl;         
+      }
+      catch(std::exception & e)
+      {
+         cout << e.what() << endl;
+      }
+
+      
+   cout << "<FORM METHOD=\"GET\" ACTION=\"createcard.cgi?id=" << id << "\">\n"
+        << " <TEXTAREA NAME=\"message\" ROWS=6 COLS=60> "
+        <<   "Write your message here.."
+        << " </TEXTAREA>\n"
+        << " <INPUT TYPE=\"submit\" /> \n"
+        << "</FORM>\n";
+   
+   }
+   else
+      cout << "Du måste ange ett positivet heltal som id" << endl;
+      
+   
+      
    cout     << "</BODY></HTML>" << endl;
 
-   
+    
 
    return 0;
 }
